e90a5fe8bf8ad964c8adfdf979a536ef54f77d39
mercedesbenz00 <sony010521@gmail.com>
2022-05-19 22:37:41 -0500

    Update footer.jsx
    Corrected a few typos 05/19/2022

--- components/footer.jsx
+++ components/footer.jsx
@@ -10,7 +10,7 @@ export default class Footer extends Component {
             <Grid>
               <Grid.Column width={16} textAlign='center'>
                 <Icon name='birthday' />
-                OpenPOA (OP) was once known as POA Network, which launched on 12/15/17
+                OpenPOA Network (POA) originally deployed as POA Network, launched on 12/15/17
               </Grid.Column>
             </Grid>
           </Container>
